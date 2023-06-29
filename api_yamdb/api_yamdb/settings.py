7eb1e66d96f92c0f11210bc3a2fc773e7ff97d50
Дмитрий Дубиков <workvandomar@gmail.com>
2023-06-29 15:47:09 +0300

    false

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='set_your_secret_key_in_.env_file')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
